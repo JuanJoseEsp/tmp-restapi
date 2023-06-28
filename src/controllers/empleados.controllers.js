f41b5f76a6cdeadb144b50761fc7a460ec6cea19
juan Espinosa <juanjose14413@gmail.com>
2023-06-28 17:57:24 -0600

    Se creo la funcion obtenerEmpleados

--- src/controllers/empleados.controllers.js
+++ src/controllers/empleados.controllers.js
@@ -1,7 +1,16 @@
 import { pool } from "../db.js";
 
-export const obtenerEmpleados = (req, res) => {
-    res.json({
-        mensaje: 'si jala wey'
+export const obtenerEmpleados = async (req, res) => {
+  try {
+    const [rows] = await pool.query("SELECT * FROM empleado");
+    res.json(rows);
+  } catch (error) {
+    return res.status(500).json({
+        mensaje: "Algo salio mal"
     });
+  }
+};
+
+export const crearEmpleado = (req, res) =>{
+
 };
